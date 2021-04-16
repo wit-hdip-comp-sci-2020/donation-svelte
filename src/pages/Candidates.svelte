e96a75b66601609057cc4a773f9a88a96d095368
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 12:17:19 +0100

    new pages for candidates and donations

---
+++ src/pages/Candidates.svelte
@@ -0,0 +1,16 @@
+<script>
+  import CandidateList from "../components/CandidateList.svelte";
+  import homer4 from "/src/assets/homer4.jpeg";
+</script>
+
+<div class="uk-text-center" uk-grid>
+  <div class="uk-width-1-2@m ">
+    <img width="300" src="{homer4}" alt="homer">
+  </div>
+  <div class="uk-width-1-2@m">
+    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
+      <h3 class="uk-card-title uk-text-center">Make your donation</h3>
+      <CandidateList/>
+    </div>
+  </div>
+</div>
---
+++ src/pages/Donations.svelte
@@ -0,0 +1,16 @@
+<script>
+  import DonationList from "../components/DonationList.svelte";
+  import homer5 from "/src/assets/homer5.jpg";
+</script>
+
+<div class="uk-text-center" uk-grid>
+  <div class="uk-width-1-2@m ">
+    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
+      <h3 class="uk-card-title uk-text-center">Make your donation</h3>
+      <DonationList/>
+    </div>
+  </div>
+  <div class="uk-width-1-2@m">
+    <img width="300" src="{homer5}" alt="homer">
+  </div>
+</div>
