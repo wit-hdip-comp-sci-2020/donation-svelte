0912ac4d64ab77280503c7fb2b8a64c599c6b3ff
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 18:39:02 +0100

    new LoginForm component

---
+++ src/components/LoginForm.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { push } from "svelte-spa-router";
+
+  let email = ""
+  let password = "";
+  let errorMessage = "";
+
+  async function login() {
+    push("/candidates");
+  }
+</script>
+
+<form on:submit|preventDefault={login}>
+  <div class="uk-margin uk-text-left">
+    <label class="uk-form-label">Email</label>
+    <div class="uk-inline uk-width-1-1">
+      <span class="uk-form-icon" uk-icon="icon: mail"></span>
+      <input bind:value={email} class="uk-input uk-form-large" type="text" name="email">
+    </div>
+  </div>
+  <div class="uk-margin">
+    <div class="uk-inline uk-width-1-1">
+      <span class="uk-form-icon" uk-icon="icon: lock"></span>
+      <input bind:value={password} class="uk-input uk-form-large" type="password" name="password">
+    </div>
+  </div>
+  <div class="uk-margin">
+    <button class="uk-button uk-button-primary uk-button-large uk-width-1-1">Login</button>
+  </div>
+  {#if errorMessage}
+    <div class="uk-text-left uk-text-small">
+      {errorMessage}
+    </div>
+  {/if}
+</form>
