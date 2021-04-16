2c95c2bef22670d5cc168d29fecf0b2f289c4597
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 10:05:57 +0100

    fetch donations method

--- src/donation-service.js
+++ src/donation-service.js
@@ -1,5 +1,6 @@
 export class DonationService {
   candidateList = [];
+  donationList = [];
   baseUrl = "";
 
   constructor(baseUrl) {
@@ -11,6 +12,16 @@ export class DonationService {
       const response = await fetch(this.baseUrl + "/api/candidates")
       this.candidateList = await response.json();
       return this.candidateList;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async getDonations() {
+    try {
+      const response = await fetch(this.baseUrl + "/api/donations")
+      this.donationList = await response.json();
+      return this.donationList;
     } catch (error) {
       return [];
     }
