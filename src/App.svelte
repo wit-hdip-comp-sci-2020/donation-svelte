e93dfbde194b705e74263f44680699ebdaeace4d
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 17:50:35 +0100

    remove navBar from app

--- src/App.svelte
+++ src/App.svelte
@@ -12,20 +12,9 @@
     "/candidates": Candidates,
     "/donations": Donations,
   }
-  const navBar = [{
-    title: "Donations",
-    icon: "fas fa-hand-holding-usd fa-3x",
-    colour: "color:rgb(153, 196, 74)",
-    link: "/#/donations",
-  }, {
-    title: "Candidates",
-    icon: "fas fa-clipboard-list fa-3x",
-    colour: "color:rgb(63, 122, 139)",
-    link: "/#/candidates",
-  }];
 </script>
 
 <div class="uk-container">
-  <Navigator {navBar}/>
+  <Navigator/>
   <Router {routes}/>
 </div>
