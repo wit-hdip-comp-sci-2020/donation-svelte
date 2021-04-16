acc523d39c28008d016e90ce021d17c7311646be
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 13:17:39 +0100

    adjust title of list components

--- src/pages/Candidates.svelte
+++ src/pages/Candidates.svelte
@@ -9,7 +9,6 @@
   </div>
   <div class="uk-width-1-2@m">
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
-      <h3 class="uk-card-title uk-text-center">Make your donation</h3>
       <CandidateList/>
     </div>
   </div>
--- src/pages/Donations.svelte
+++ src/pages/Donations.svelte
@@ -6,7 +6,6 @@
 <div class="uk-text-center" uk-grid>
   <div class="uk-width-1-2@m ">
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
-      <h3 class="uk-card-title uk-text-center">Make your donation</h3>
       <DonationList/>
     </div>
   </div>
