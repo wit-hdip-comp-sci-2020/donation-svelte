df32232b304728b3c7e2bf967fd9b5c65b9d32d2
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 13:27:16 +0100

    rework app to use the router

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,17 @@
   import Candidates from "./pages/Candidates.svelte";
   import Donations from "./pages/Donations.svelte";
   import Navigator from "./components/Navigator.svelte";
+  import Router from "svelte-spa-router";
 
   setContext("DonationService", new DonationService("http://localhost:4000"));
+
+  let routes = {
+    "/candidates": Candidates,
+    "/donations": Donations,
+  }
 </script>
 
 <div class="uk-container">
   <Navigator/>
-  <Candidates/>
-  <Donations/>
+  <Router {routes} />
 </div>
