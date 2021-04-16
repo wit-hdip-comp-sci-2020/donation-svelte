a2b7d592e15c5aac24eefee4e1f52e5d5c061a09
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 12:17:38 +0100

    include pages instead of components in app

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
   import {setContext} from "svelte";
-  import CandidateList from "./components/CandidateList.svelte"
   import {DonationService} from "./services/donation-service";
-  import DonationList from "./components/DonationList.svelte";
+  import Candidates from "./pages/Candidates.svelte";
+  import Donations from "./pages/Donations.svelte";
 
   setContext("DonationService", new DonationService("http://localhost:4000"));
 </script>
 
 <div class="uk-container">
   <h1> Donation Client Application </h1>
-  <CandidateList/>
-  <DonationList/>
+  <Candidates/>
+  <Donations/>
 </div>
