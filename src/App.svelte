924403ed431995684fcedda6da08fa2733c4cbbf
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-16 13:06:41 +0100

    include Navgator in App

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,13 @@
   import {DonationService} from "./services/donation-service";
   import Candidates from "./pages/Candidates.svelte";
   import Donations from "./pages/Donations.svelte";
+  import Navigator from "./components/Navigator.svelte";
 
   setContext("DonationService", new DonationService("http://localhost:4000"));
 </script>
 
 <div class="uk-container">
-  <h1> Donation Client Application </h1>
+  <Navigator/>
   <Candidates/>
   <Donations/>
 </div>
