f04a12fa12d79b5aebd1bcd8518d7bef51ca1c85
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-19 10:29:30 +0100

    donate form

---
+++ src/pages/Donate.svelte
@@ -0,0 +1,24 @@
+<script>
+  import homer from "/src/assets/homer.png";
+  import {navBar, mainBar, subTitle, title} from "../stores"
+  import DonateForm from "../components/DonateForm.svelte";
+
+  title.set("Donation Services Inc.");
+  subTitle.set("Make a Donation!");
+  navBar.set({
+    bar: mainBar
+  });
+</script>
+
+<div class="uk-flex-middle uk-text-center" uk-grid>
+  <div class="uk-width-1-2@m ">
+    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
+      <img width="300" src="{homer}" alt="homer">
+    </div>
+  </div>
+  <div class="uk-width-1-2@m ">
+    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
+      <DonateForm/>
+    </div>
+  </div>
+</div>
