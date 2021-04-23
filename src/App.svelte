e70df80e30af7186ef57a09fde2e1516a1b5279d
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-23 08:25:04 +0100

    routes for signup + settings

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
   import {DonationService} from "./services/donation-service";
   import Main from "./pages/Main.svelte"
   import Login from "./pages/Login.svelte"
+  import Signup from "./pages/Signup.svelte"
   import Candidates from "./pages/Candidates.svelte";
   import Donations from "./pages/Donations.svelte";
   import Donate from "./pages/Donate.svelte";
+  import Settings from "./pages/Settings.svelte";
   import Navigator from "./components/Navigator.svelte";
   import Router from "svelte-spa-router";
 
@@ -14,9 +16,11 @@
   let routes = {
     "/" : Main,
     "/login": Login,
+    "/signup" : Signup,
     "/candidates": Candidates,
     "/donations": Donations,
     "/donate": Donate,
+    "/settings" : Settings,
     "/logout" : Main
   }
 </script>
