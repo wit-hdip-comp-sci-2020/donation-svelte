911c8fd978c9fd200ee70812e21fcfc4150b9b46
Eamonn de Leastar <edeleastar@wit.ie>
2021-04-25 07:57:32 +0100

    display email instead of name in title

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -22,7 +22,7 @@
           <i class="fas fa-donate fa-3x" style="color:rgb(95, 96, 173)" title="Source repo" pos="bottom" uk-tooltip></i>
         </div>
         <div class="uk-width-expand@m uk-text-left">
-          <div class="uk-text-muted uk-text-center" style="font-size:xx-small">{$user.firstName} {$user.lastName}</div>
+          <div class="uk-text-muted uk-text-center" style="font-size:xx-small">{$user.email} </div>
         </div>
       </a>
     </div>
