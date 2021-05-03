90c1454530903e6fd5fbd4532556fa526aa160ff
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-03 18:35:14 +0100

    display donation method bar chart

--- src/pages/Analysis.svelte
+++ src/pages/Analysis.svelte
@@ -1,7 +1,10 @@
 <script>
   import homer from "/src/assets/homer.png";
-  import {navBar, mainBar, subTitle, title} from "../stores"
+  import {mainBar, navBar, subTitle, title} from "../stores"
   import Chart from 'svelte-frappe-charts';
+  import {getContext, onMount} from "svelte";
+
+  const donationService = getContext("DonationService");
 
   title.set("Donation Services Inc.");
   subTitle.set("Analysis");
@@ -10,13 +13,24 @@
   });
 
   let data = {
-    labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
+    labels: ["paypal", "direct"],
     datasets: [
       {
-        values: [10, 12, 3, 9, 8, 15, 9]
+        values: [0, 0]
       }
     ]
   };
+
+  onMount(async () => {
+    let donationList = await donationService.getDonations();
+    donationList.forEach(donation => {
+      if (donation.method == "paypal") {
+        data.datasets[0].values[0] += donation.amount
+      } else if (donation.method == "direct") {
+        data.datasets[0].values[1] += donation.amount
+      }
+    })
+  });
 </script>
 
 <div class="uk-flex-middle uk-text-center" uk-grid>
@@ -27,7 +41,7 @@
   </div>
   <div class="uk-width-1-2@m ">
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge ">
-      <Chart data={data} type="line" />
+      <Chart data={data} type="bar"/>
     </div>
   </div>
 </div>
