5718ceea5920c3ba65269b137bf2727d4e636f49
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-04 10:16:41 +0100

    move to using map/reduce - introduce donor chart

--- src/pages/Analysis.svelte
+++ src/pages/Analysis.svelte
@@ -13,7 +13,6 @@
   });
 
   let candidates = [];
-  let donationsList = [];
   let paymentData = {
     labels: ["paypal", "direct"],
     datasets: [
@@ -32,27 +31,37 @@
     ]
   }
 
+  let donorData = {
+    labels: [],
+    datasets: [
+      {
+        values: []
+      }
+    ]
+  }
   async function refreshCharts() {
     let donationList = await donationService.getDonations();
-    donationList.forEach(donation => {
-      if (donation.method == "paypal") {
-        paymentData.datasets[0].values[0] += donation.amount
-      } else if (donation.method == "direct") {
-        paymentData.datasets[0].values[1] += donation.amount
-      }
-    })
-    donationsData.datasets[0].values = [];
+
+    let sumPaypal = donationList.filter(donation => donation.method == "paypal").reduce((a, b) => ({amount: a.amount + b.amount}));
+    paymentData.datasets[0].values[0] = sumPaypal.amount;
+    let sumDirect = donationList.filter(donation => donation.method == "direct").reduce((a, b) => ({amount: a.amount + b.amount}));
+    paymentData.datasets[0].values[1] = sumDirect.amount;
+
     candidates.forEach((candidate, i) => {
-      donationList.forEach(donation => {
-        if (donation.candidate._id == candidate._id) {
-          if (!donationsData.datasets[0].values[i]) {
-            donationsData.datasets[0].values[i] = donation.amount;
-          } else {
-            donationsData.datasets[0].values[i] += donation.amount;
-          }
-        }
-      });
+      let total = donationList.filter(donation => donation.candidate._id == candidate._id).reduce((a, b) => ({amount: a.amount + b.amount}));
+      donationsData.datasets[0].values[i] = total.amount;
     });
+
+    let donors = new Map();
+    donationList.forEach(donation => {donors.set(donation.donor._id, donation.donor)})
+    let i = 0;
+    donorData.labels = [];
+    for (const [id, donor] of donors.entries()) {
+      donorData.labels.push(`${donor.lastName}, ${donor.firstName}`);
+      let sumDonated = donationList.filter(donation => donation.donor._id == id).reduce((a, b) => ({amount: a.amount + b.amount}));
+      donorData.datasets[0].values[i] = sumDonated.amount;
+      i++;
+    }
   }
 
   function justDonated() {
@@ -72,14 +81,21 @@
 <div class="uk-flex-middle uk-text-center" uk-grid>
   <div class="uk-width-1-2@m ">
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge uk-margin">
+      <h3> Payment Methods </h3>
       <Chart data={paymentData} type="pie"/>
     </div>
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge uk-margin">
+      <h3> Donations by Candidate </h3>
       <Chart data={donationsData} type="bar"/>
     </div>
   </div>
   <div class="uk-width-1-2@m ">
     <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge uk-margin">
+      <h3> Donations by Donor </h3>
+      <Chart data={donorData} type="donut"/>
+    </div>
+    <div class="uk-card uk-card-default uk-card-body uk-box-shadow-large uk-width-2xlarge uk-margin">
+      <h3> Make a Donation </h3>
       <DonateForm {justDonated}/>
     </div>
   </div>
