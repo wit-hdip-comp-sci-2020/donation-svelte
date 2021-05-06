2bc286b0e446eef06c6dc150561535145d856bac
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-06 11:20:29 +0100

    leaflet service class

---
+++ src/services/leaflet-map.ts
@@ -0,0 +1,102 @@
+import * as L from 'leaflet';
+import Map = L.Map;
+import Layer = L.Layer;
+import LayersObject = L.Control.LayersObject;
+import LayerGroup = L.LayerGroup;
+import LayerControl = L.Control.Layers;
+
+export interface Location {
+  lat: number;
+  lng: number;
+}
+
+export interface MapConfig {
+  location: Location;
+  zoom: number;
+  minZoom: number;
+}
+
+export class LeafletMap {
+  imap: Map;
+  control: LayerControl;
+  overlays: LayersObject = {};
+
+  // https://leaflet-extras.github.io/leaflet-providers/preview/
+
+  baseLayers = {
+    Terrain: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 17,
+      attribution:
+          'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    })
+  };
+
+  constructor(id: string, descriptor: MapConfig, activeLayer = '') {
+    let defaultLayer = this.baseLayers.Terrain;
+    if (activeLayer) {
+      defaultLayer = this.baseLayers[activeLayer];
+    }
+    this.imap = L.map(id, {
+      center: [descriptor.location.lat, descriptor.location.lng],
+      zoom: descriptor.zoom,
+      minZoom: descriptor.minZoom,
+      zoomControl: false,
+      layers: [defaultLayer]
+    });
+  }
+
+  addLayer(title: string, layer: Layer) {
+    this.overlays[title] = layer;
+    this.imap.addLayer(layer);
+  }
+
+  addLayerGroup(title: string) {
+    this.overlays[title] = L.layerGroup([]);
+    this.imap.addLayer(this.overlays[title]);
+  }
+
+  showLayerControl() {
+    this.control = L.control.layers(this.baseLayers, this.overlays).addTo(this.imap);
+  }
+
+  showZoomControl(position = 'topleft') {
+    L.control
+        .zoom({
+          position: position
+        })
+        .addTo(this.imap);
+  }
+
+  moveTo(zoom: number, location: Location) {
+    this.imap.setZoom(zoom);
+    this.imap.panTo(new L.LatLng(location.lat, location.lng));
+  }
+
+  zoomTo(location: Location) {
+    this.imap.setView(new L.LatLng(location.lat, location.lng), 8);
+  }
+
+  addMarker(location: Location, popupText = '', layerTitle = 'default') {
+    let group: LayerGroup;
+    let marker = L.marker([location.lat, location.lng]);
+    if (popupText) {
+      var popup = L.popup({ autoClose: false, closeOnClick: false });
+      popup.setContent(popupText);
+      marker.bindPopup(popup);
+    }
+    if (!this.overlays[layerTitle]) {
+      group = L.layerGroup([]);
+      this.overlays[layerTitle] = group;
+      this.imap.addLayer(group);
+    } else {
+      group = this.overlays[layerTitle] as LayerGroup;
+    }
+    marker.addTo(group);
+  }
+
+  invalidateSize() {
+    this.imap.invalidateSize();
+    let hiddenMethodMap = this.imap as any;
+    hiddenMethodMap._onResize();
+  }
+}
